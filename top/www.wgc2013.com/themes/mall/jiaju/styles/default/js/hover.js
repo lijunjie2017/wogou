28a701dc45abfa673c9f3916004846d558ccfbac
lijunjie <13232807112@163.com>
2018-07-27 17:12:10 +0800

    top

---
+++ top/www.wgc2013.com/themes/mall/jiaju/styles/default/js/hover.js
@@ -0,0 +1,33 @@
+﻿// JavaScript Document
+<!--
+function setTab(name,cursel,n){
+for(i=1;i<=n;i++){
+var menu=document.getElementById(name+i);
+var con=document.getElementById("con_"+name+"_"+i);
+menu.className=i==cursel?"hover":"";
+con.style.display=i==cursel?"block":"none";
+}
+}
+//-->
+
+<!--
+function setTab(name,cursel,n){
+for(i=1;i<=n;i++){
+var menu=document.getElementById(name+i);
+var con=document.getElementById("con_"+name+"_"+i);
+menu.className=i==cursel?"com_hov":"";
+con.style.display=i==cursel?"block":"none";
+}
+}
+//-->
+
+<!--
+function setTab1(name,cursel,n){
+for(i=1;i<=n;i++){
+var menu=document.getElementById(name+i);
+var con=document.getElementById("con_"+name+"_"+i);
+menu.className=i==cursel?"tc_hov":"";
+con.style.display=i==cursel?"block":"none";
+}
+}
+//-->
